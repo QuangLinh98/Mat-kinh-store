df8e3bc90283fdaf7e6b6c05f9e7f6d6f987ed79
Nguyen Quang Linh <nguyenlinhak4@gmail.com>
2024-01-07 11:05:33 +0700

    thay đổi trang thank

--- resources/views/checkout/thank.blade.php
+++ resources/views/checkout/thank.blade.php
@@ -5,7 +5,7 @@
             <h1 class="display-4">Thank You!</h1>
             <p class="lead">Your message has been received.</p>
             <hr class="my-4">
-            <p>We appreciate your feedback.</p>
+            <p>We appreciate your feedback. See you again </p>
             <a class="btn btn-primary btn-lg" href="{{ route('home') }}" role="button">Back to Home</a>
         </div>
     </div>
